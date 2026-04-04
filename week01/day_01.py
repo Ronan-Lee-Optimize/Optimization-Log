edd0778f8c3e7211f174aa5e1fbc8ea1c0b4bbe2
Ronan-Lee-Optimize <qwertyabcd162517@gmail.com>
2026-04-04 23:34:31 +0900

    Day 01:  variables, print, input review
    Fresh new start!

---
+++ week01/day_01.py
@@ -0,0 +1,15 @@
+# Day 01: variables, print, input
+# opt-log | consistency over perfection.
+
+name = "Ronan Lee"
+goal = "Transfer to UC - IE"
+daysleft = 229
+
+
+print("Name: ", name)
+print("Goal: ", goal)
+print("Days till CSAT:", daysleft)
+
+
+study =  input("What did u study today?")
+print("Logged:", study)
