1e994b52f7074046b845fa33ccbfbf3caf8db8cf
Ronan-Lee-Optimize <qwertyabcd162517@gmail.com>
2026-04-06 23:57:15 +0900

    for loop

---
+++ week01/day_03.py
@@ -0,0 +1,21 @@
+# day 03 - for loop
+# opt-log | consistency over perfection.
+
+# repeat from 0 to 4
+for i in range (5):
+    print("day", i)
+
+
+# range(start, end): repeat from 1 to 5 (end not included)
+for i in range (1,6):
+    print("day", i)
+
+
+# loop though a list one by one
+subjects = ["math", "physics", "literature"]
+
+for subject in subjects:
+    print("I'm studying",subject)
+
+
+# I'm hacking python 🔥
