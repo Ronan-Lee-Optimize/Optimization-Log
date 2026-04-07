dfec3e2e74cf2534a4be4af1929f2ef919eb1849
Ronan Lee <qwertyabcd162517@gmail.com>
2026-04-08 00:12:05 +0900

    while loop and += operator
    Refactor while loop to use += operator for incrementing count.

---
+++ week01/day_04.py
@@ -0,0 +1,19 @@
+# day 04 - while loop and += operator
+# opt-log | consistency over perfection.
+
+count = 0
+
+while count < 5:
+    print("count:", count)
+    count = count + 1
+
+
+
+count = 0
+
+while count < 5:
+    print("count:",count)
+    count += 1 # shorthand for count = count + 1
+
+
+# IDLE is the only place that i can escape form the 5 choice and feel real freedom
