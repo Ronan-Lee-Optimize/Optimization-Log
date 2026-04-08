8aff86bfd7033237301e3528c9a179e11d45cfdb
Ronan Lee <qwertyabcd162517@gmail.com>
2026-04-08 23:57:47 +0900

    functions

---
+++ week01/day_05.py
@@ -0,0 +1,15 @@
+# day 05 - functions
+# opt-log | consistency over perfection.
+
+
+def abc():
+    print("hello, world") # define function
+
+abc() # call the function
+
+
+
+def abc(name):
+    print("hello,",name) # function with a parameter
+
+abc("ronan") # pass a value into the function
