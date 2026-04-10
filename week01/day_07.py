dee9ed59ebcc315ff0a2b5def78b315aa1c21188
Ronan Lee <qwertyabcd162517@gmail.com>
2026-04-10 23:57:26 +0900

    Add student dictionary with details and print statements

---
+++ week01/day_07.py
@@ -0,0 +1,12 @@
+# Day 07 - dictionaries
+# opt-log | Consistency over perfection.
+
+student = {
+    "name": "Ronan",
+    "goal": "UC Transfer",
+    "days_studied": 7
+}
+
+print(student["name"])        # access by key
+print(student["goal"])        # access by key
+print(student["days_studied"]) # access by key
