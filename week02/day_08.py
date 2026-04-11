07cfffd0929a6b0e57e8543b1e80dcc0adf82a5d
Ronan Lee <qwertyabcd162517@gmail.com>
2026-04-12 00:05:35 +0900

    review day 01: variables, print, input (applied)

---
+++ week02/day_08.py
@@ -0,0 +1,17 @@
+# day 08 - review day 01: variables, print, input (applied)
+# opt-log | consistency over perfection.
+
+name = input("whats ur name ")
+goal = input("whats ur goal ")
+days = int(input("how long did u studied "))
+
+
+
+print("name:",name)
+print("goal:",goal)
+print("days studied:", days)
+print("keep goin", name,"u fr on ur way, dw u lit🔥.") # applied
+
+
+
+print("dw, u r doing good. believe in urslf my big man")
