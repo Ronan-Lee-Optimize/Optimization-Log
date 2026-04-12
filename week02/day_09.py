57b186f76009df37436bd34ac2c5b86ac1100d32
Ronan Lee <qwertyabcd162517@gmail.com>
2026-04-12 23:58:26 +0900

    review day 02: if/else (applied)
    Added a script to review if/else statements with user input on study hours.

---
+++ week02/day_09.py
@@ -0,0 +1,27 @@
+# day 09 - review day 02: if/else (applied)
+# opt-log | consistency over perfection.
+
+
+hours = int(input("how many hours did you study today? "))
+
+
+print()  # empty line
+
+
+print("lets see the results 😋")
+
+
+print()  # empty line
+
+
+if hours >= 8:
+    print("beast mode🐯 def u locked in")
+
+elif hours >= 5:
+    print("solid day. keep it up🫡.")
+    
+elif hours >= 2:
+    print("kay push harder tmw😛")
+    
+else:
+    print("u'd better do ur best next time😪")
