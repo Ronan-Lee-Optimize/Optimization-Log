c6c6eb0fdaf3306f7712af74dc990a2987401e4a
Ronan Lee <qwertyabcd162517@gmail.com>
2026-04-14 00:04:52 +0900

    Update day_10.py

--- week02/day_10.py
+++ week02/day_10.py
@@ -1,8 +1,15 @@
-# Day 10 - Review Day 03: for loop (Applied)
-# opt-log | Consistency over perfection.
+# day 10 - review day 03: for loop (applied)
+# opt-log | consistency over perfection.
 
-subjects = ["Math", "English", "Korean", "Science"]
+subjects = ["math", "english", "korean", "science"]
 hours = [3, 2, 4, 1]
 
-for i in range(len(subjects)):
-    print("Studied", subjects[i], "for", hours[i], "hour(s)")  # loop through two lists
+
+
+for i in range(len(subjects)):  # len() returns the length of the list
+    
+    print("studied", subjects[i], "for", hours[i], "hours(s)")  # access both lists by index
+
+
+
+# pre csat is coming again 🫩 ARGRHRGRHRRGH
