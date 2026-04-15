1fd3e80287c995aeb2372710575b29bf2ddf5802
Ronan Lee <qwertyabcd162517@gmail.com>
2026-04-16 02:35:13 +0900

    Implement functions for greeting and multiplication
    Added functions for greeting and multiplication.

---
+++ week02/day_12.py
@@ -0,0 +1,29 @@
+# day 12 - day_05 review | functions
+# opt-log | consistency over perfection.
+
+
+
+def sayhi():
+    print("hi wrld")
+
+sayhi()
+
+
+
+def greet(name):        # function with a parameter
+    print("hi", name)
+
+greet("ronan")
+
+
+
+def multiply(a, b):     # function with return value
+    return a * b
+
+
+
+result = multiply(4, 6)
+
+
+
+print("Result:", result)
