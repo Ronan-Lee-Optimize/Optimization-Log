528fa1422cf7c3318f31906b0dc8aee8dec3b734
Ronan Lee <qwertyabcd162517@gmail.com>
2026-04-18 20:30:13 +0900

    strings

---
+++ week03/day_15.py
@@ -0,0 +1,24 @@
+# day 15 - strings
+# opt-log | consistency over perfection.
+
+name = "ronan"
+
+
+print(len(name))        # length of the string
+
+print(name.upper())     # convert to uppercase
+
+print(name.lower())     # convert to lowercase
+
+print(name[0])          # first character (indexing)
+print()
+
+
+sentence = "i luv python"
+
+
+print(sentence.replace("python", "coding"))  # replace a word
+
+print(sentence.split(" "))                   # split into a list
+
+# new week has started 🔥
