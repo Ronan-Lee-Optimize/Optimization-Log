9ee4bb5c22c4cde12c8e284f6e397dd70d01aca0
Ronan Lee <qwertyabcd162517@gmail.com>
2026-04-20 23:56:59 +0900

    day 17: tuples and sets
    Added a new Python file demonstrating tuples and sets.

---
+++ week03/day_17.py
@@ -0,0 +1,16 @@
+#day 17 - tuples & sets
+# opt-log | consistency over perfection.
+
+# tuple: ordered, cannot be changed
+my_tuple = ("math", "english", "korean")
+
+
+print(my_tuple[0])       # access by index
+
+print(len(my_tuple))     # length of tuple
+
+# set: unordered, no duplicates
+my_set = {"math", "english", "korean", "math"}  # Math appears twice
+
+
+print(my_set)            # duplicates are removed automatically
