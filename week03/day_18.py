ebc6d24a99a361ccd66aa8711c3caac5d2e31923
Ronan Lee <qwertyabcd162517@gmail.com>
2026-04-21 22:00:16 +0900

    day 18: nested lists

---
+++ week03/day_18.py
@@ -0,0 +1,17 @@
+# day 18 - nested lists
+# opt-log | consistency over perfection.
+
+sched = [
+    ["math", 3],       # subj and hrs
+    ["english", 2],
+    ["korean", 4]
+]
+
+
+
+for item in sched:
+    print("subj:", item[0], "- hrs:", item[1])  # access inner list
+
+
+
+# planning to make my own calculator w python lol
