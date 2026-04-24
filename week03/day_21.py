3e2800c70f46862a331b88e6ca4276d3011772d3
Ronan Lee <qwertyabcd162517@gmail.com>
2026-04-24 23:58:23 +0900

    Add f-strings examples in day_21.py

---
+++ week03/day_21.py
@@ -0,0 +1,13 @@
+# day 21 - f-strings
+# opt-log | consistency over perfection.
+
+  
+name = "ronan"
+days = 21
+
+  
+print(f"name: {name}")          # variable inside string
+  
+print(f"days studied: {days}")  # no need for str() or +
+  
+print(f"{name} has studied for {days} days. keep goin bro")  # combined
