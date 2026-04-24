a42383bac7726c4a6756b529c9e358909ec1e80e
Ronan Lee <qwertyabcd162517@gmail.com>
2026-04-25 00:06:35 +0900

    day 21: f-strings
    Added a comment about the upcoming calculator project.

--- week03/day_21.py
+++ week03/day_21.py
@@ -11,3 +11,5 @@ print(f"name: {name}")          # variable inside string
 print(f"days studied: {days}")  # no need for str() or +
   
 print(f"{name} has studied for {days} days. keep goin bro")  # combined
+
+# we're making calculator from tmw. YAY TMR IS WEEKEND FINALLY🔥
