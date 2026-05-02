3d74ca46ca4d457b70c8e32aa33afa740ebba747
Ronan Lee <qwertyabcd162517@gmail.com>
2026-05-02 23:30:14 +0900

    day 29: try/except error handling

---
+++ week04/day_29.py
@@ -0,0 +1,11 @@
+# day 29 - try/except: error handling
+# opt-log | consistency over perfection.
+
+try:
+    num = int(input("enter a number: "))
+    print(f"your number is: {num}")
+    
+except:
+    print("dats not a number bruh")  # runs if error occurs
+
+# uhhhhhhhhhhhh
