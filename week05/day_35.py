85fb41cb1286201587f393dd5e7fcd3b253a9ff7
Ronan Lee <qwertyabcd162517@gmail.com>
2026-05-08 22:46:46 +0900

    day 35: *args

---
+++ week05/day_35.py
@@ -0,0 +1,13 @@
+# day 35 - *args
+# opt-log | Consistency over perfection.
+
+def total(*args):
+    print(args)        # args is a tuple
+    print(sum(args))   # sum all values
+
+
+
+total(1, 2, 3)
+total(10, 20, 30, 40)
+
+# TGIF but IM DYINGGGGG UGH I WANNA SLEEP
