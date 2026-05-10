f64b3f594a8fa6d13251363f5812e24c0e6663fd
Ronan Lee <qwertyabcd162517@gmail.com>
2026-05-10 21:55:52 +0900

    day 37: review try/except applied
    Implemented a loop that takes user input and handles exceptions for non-numeric input and division by zero.

---
+++ week06/day_37.py
@@ -0,0 +1,20 @@
+# day 37 - review: try/except (applied)
+# opt-log | consistency over perfection.
+
+while True:
+    try:
+        num = int(input("enter a number (0 to quit): "))
+        if num == 0:
+            print("program stopped")
+            break
+        
+        print(f"result: {100 / num}")
+
+
+        
+    except ValueError:
+        print("not a number")
+
+        
+    except ZeroDivisionError:
+        print("cannot divide by 0")
