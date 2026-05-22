76e4369079e216c0454caabc8077c6eedcb46bd4
Ronan Lee <qwertyabcd162517@gmail.com>
2026-05-22 23:57:42 +0900

    Add student class to track study days

---
+++ week07/day_49.py
@@ -0,0 +1,19 @@
+name = input("enter ur name: ")
+days = int(input("enter days u studied: "))
+
+
+    
+class stu:
+    def __init__(aaa, name, days):
+        
+        aaa.name = name
+        aaa.days = days
+
+
+
+ronan = stu(name, days)
+
+
+print(ronan.name, "has studied")
+print(ronan.days, "days")
+
