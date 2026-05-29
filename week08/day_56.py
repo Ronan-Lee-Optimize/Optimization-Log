db0fe9d2e563da1b680d04061b3dc1dde93490a0
Ronan Lee <qwertyabcd162517@gmail.com>
2026-05-29 23:58:01 +0900

    day 56: build my own

---
+++ week08/day_56.py
@@ -0,0 +1,8 @@
+# day 56 - make my own thing
+# opt-log | consistency over perfection.
+
+class stu:
+    def __init__(self, math, english):
+        self.math = math
+        self.english = english
+        print(f"my math score is {self.math}, abd my english score is {self.english}")
