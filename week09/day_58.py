8805ac736a8a09aa9773813741119332d9b9859f
Ronan Lee <qwertyabcd162517@gmail.com>
2026-05-31 23:51:02 +0900

    day 58: modules - random

---
+++ week09/day_58.py
@@ -0,0 +1,7 @@
+# day 58 - modules: random
+# opt-log | consistency over perfection.
+
+import random
+
+print(random.randint(1, 10))         # random number 1-10
+print(random.choice(["math", "english", "korean"]))  # random item from list
