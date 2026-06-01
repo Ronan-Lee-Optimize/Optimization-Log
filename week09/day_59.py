91684549b0aa581b953c6965bbeebb2028a8dd2b
Ronan Lee <qwertyabcd162517@gmail.com>
2026-06-01 22:57:07 +0900

    day 59: modules - datetime

---
+++ week09/day_59.py
@@ -0,0 +1,13 @@
+# day 59 - modules: datetime
+# opt-log | consistency over perfection.
+
+import datetime
+
+now = datetime.datetime.now()  # current date and time
+
+
+print("RONAN CLOCK")
+print(now)
+print(now.year)
+print(now.month)
+print(now.day)
