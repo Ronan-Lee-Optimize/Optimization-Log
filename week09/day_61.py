6352aa835de99decc335bdb8ef1219ac5d908662
Ronan Lee <qwertyabcd162517@gmail.com>
2026-06-03 23:27:11 +0900

    day 61: modules - math

---
+++ week09/day_61.py
@@ -0,0 +1,10 @@
+# day 61 - modules: math
+# opt-log | consistency over perfection.
+
+import math
+
+print(math.pi)          # pi
+print(math.sqrt(25))    # square root
+print(math.floor(3.7))  # round down
+print(math.ceil(3.2))   # round up
+print(math.pow(2, 10))  # 2 to the power of 10
