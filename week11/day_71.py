26b85e7102cebcd8e85f6ceab92814838f766809
Ronan Lee <qwertyabcd162517@gmail.com>
2026-06-13 23:21:13 +0900

    day 71: pandas introduction

---
+++ week11/day_71.py
@@ -0,0 +1,12 @@
+# day 71 - pandas: introduction
+# opt-log | consistency over perfection.
+
+import pandas as pd
+
+data = {"subject": ["math", "english", "korean"],
+        "score": [85, 92, 78]}
+
+df = pd.DataFrame(data)
+print(df)
+
+# first time trying out the visual studio code 🔥
